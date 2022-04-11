f0f8dfbbf84a8bf4fd301efcd4b935065559cbc0
brendatreto <brendatreto19@gmail.com>
2022-04-11 12:33:10 -0500

    Summary statistics

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -6,3 +6,9 @@ head(MechaCar)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar)
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar))
+
+suspension_coil <- read.csv("Suspension_Coil.csv")
+
+total_summary <- suspension_coil %>%  summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups= 'keep')
+
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups= 'keep')
