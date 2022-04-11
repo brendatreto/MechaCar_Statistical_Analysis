f6cd82958e9106fc395c9af521690d572c8da4ae
brendatreto <brendatreto19@gmail.com>
2022-04-11 11:22:33 -0500

    linear regression

---
+++ MechaCarChallenge.R
@@ -0,0 +1,8 @@
+library(dplyr)
+MechaCar <- read.csv("MechaCar_mpg.csv")
+head(MechaCar)
+
+
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar)
+
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar))
